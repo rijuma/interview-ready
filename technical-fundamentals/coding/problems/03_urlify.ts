148680623fcd88fed69ab07a6fbf50e7381a4b21
Marcos <rijuma@users.noreply.github.com>
2025-03-19 17:32:21 -0300

    Problem 03

--- technical-fundamentals/coding/problems/03_urlify.ts
+++ technical-fundamentals/coding/problems/03_urlify.ts
@@ -4,6 +4,16 @@
 // You may assume that the string has sufficient space at the end to hold the additional characters,
 // and that you are given the "true" length of the string.
 
-export default function URLify (s1 : string): string {
+export default function URLify(s1: string): string {
+  // This feels like cheating...
+  return s1.replaceAll(' ', '%20')
+}
 
+// Just an alternate way if replaceAll is not available since it's 'relatively' new.
+export function URLifyAlt(s1: string): string {
+  const output = []
+
+  for (let i = 0; i < s1.length; i++) output.push(s1[i] === ' ' ? '%20' : s1[i])
+
+  return output.join('')
 }
