71584ffe88787b60ca622529405290e1e5972394
Marcos <rijuma@users.noreply.github.com>
2025-03-19 17:20:30 -0300

    Problem 02

--- technical-fundamentals/coding/problems/02_checkPermutations.ts
+++ technical-fundamentals/coding/problems/02_checkPermutations.ts
@@ -1,5 +1,12 @@
 // 2. *Check Permutation*:
 
 // Given two strings, write a method to decide if one is a permutation of the other.
+export default function checkPermutations(s1: string, s2: string): boolean {
+  // I'm assuming the intention is to have the cleanest understandable code to solve the problem
+  // instead of the most optimized.
 
-export default function checkPermutations(s1: string, s2: string): boolean {}
+  const s1Sorted = s1.split("").sort().join("")
+  const s2Sorted = s2.split("").sort().join("")
+
+  return s1Sorted === s2Sorted
+}
