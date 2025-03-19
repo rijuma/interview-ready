db65d3b5317ffad525cb2883c0a5c33715df88ec
Marcos <rijuma@users.noreply.github.com>
2025-03-19 17:22:34 -0300

    Added prettier settings

--- technical-fundamentals/coding/problems/02_checkPermutations.ts
+++ technical-fundamentals/coding/problems/02_checkPermutations.ts
@@ -5,8 +5,8 @@ export default function checkPermutations(s1: string, s2: string): boolean {
   // I'm assuming the intention is to have the cleanest understandable code to solve the problem
   // instead of the most optimized.
 
-  const s1Sorted = s1.split("").sort().join("")
-  const s2Sorted = s2.split("").sort().join("")
+  const s1Sorted = s1.split('').sort().join('')
+  const s2Sorted = s2.split('').sort().join('')
 
   return s1Sorted === s2Sorted
 }
