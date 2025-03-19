4f8338444067cd433de7ecbe332eb1de93a5011a
Marcos <rijuma@users.noreply.github.com>
2025-03-19 18:23:16 -0300

    Problem 04

--- technical-fundamentals/coding/problems/04_palindromePermutation.ts
+++ technical-fundamentals/coding/problems/04_palindromePermutation.ts
@@ -1,4 +1,4 @@
-// 4. *Palindrome Permutation*: 
+// 4. *Palindrome Permutation*:
 
 // Given a string, write a function to check if it is a permutation of a palindrome.
 // A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
@@ -9,6 +9,32 @@
 // Output True (permutations: "taco cat", "atco cta", etc.)
 // ```
 
-export default function palindromePermutation (str: string): boolean {
+export default function palindromePermutation(str: string): boolean {
+  // First we make an array of letters removing spaces
+  const stripped = str
+    .toLowerCase()
+    .split('')
+    .filter((letter) => letter !== ' ')
 
+  // We count each letter
+  const sum = new Map<string, number>()
+
+  stripped.forEach((letter) => {
+    const count = sum.get(letter) || 0
+    sum.set(letter, count + 1)
+  })
+
+  // If the char length is odd, we can have one group of letters to be off (or to be a single letter).
+  const even = !(stripped.length % 2)
+
+  // let's get the even letter counts
+  const evens = Array.from(sum.values()).filter(
+    (count) => count % 2 === 1
+  ).length
+
+  // If the char length is even, evens must be zero to be a palindrome
+  if (even) return evens === 0
+
+  // If it's odd, then the even count must be 1, otherwise is not a palindrome
+  return evens === 1
 }
