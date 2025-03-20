4fa247f2f20a30dc634bcff0bc810c6729ed58ab
Marcos <rijuma@users.noreply.github.com>
2025-03-20 17:06:35 -0300

    Problem 07

--- technical-fundamentals/coding/problems/07_rotateMatrix.ts
+++ technical-fundamentals/coding/problems/07_rotateMatrix.ts
@@ -5,6 +5,35 @@
 
 type Matrix = number[][]
 
-export default function rotateMatrix (matrix: Matrix) {
+export default function rotateMatrix(matrix: Matrix) {
+  const size = matrix.length
 
+  for (let y = 0; y < Math.floor(size / 2); y++) {
+    for (let x = y; x < size - y - 1; x++) {
+      const swap = matrix[y][x]
+      matrix[y][x] = matrix[size - x - 1][y]
+      matrix[size - x - 1][y] = matrix[size - y - 1][size - x - 1]
+      matrix[size - y - 1][size - x - 1] = matrix[x][size - y - 1]
+      matrix[x][size - y - 1] = swap
+    }
+  }
+
+  // No return, since it mutates the reference. (Not a fan of that..)
+}
+
+export function rotateMatrixWithAux(matrix: Matrix) {
+  const size = matrix.length
+
+  // With an auxiliar matrix
+  const aux: Matrix = Array(size)
+    .fill(0)
+    .map(() => Array(size).fill(0))
+
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      aux[y][x] = matrix[size - x - 1][y]
+    }
+  }
+
+  Object.assign(matrix, aux)
 }
