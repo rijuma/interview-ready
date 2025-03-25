2e11e612ce81e57a8c346802a11b2c9ffa359947
Marcos <rijuma@users.noreply.github.com>
2025-03-25 14:52:25 -0300

    Problem 11

--- technical-fundamentals/coding/problems/11_removeDups.ts
+++ technical-fundamentals/coding/problems/11_removeDups.ts
@@ -3,13 +3,32 @@
 // Write code to remove duplicates from an unsorted linked list. FOLLOW UP
 // How would you solve this problem if a temporary buffer is not allowed?
 //
-// 1 -> 2 -> 2-> 2 -> 4
+// 1 -> 2 -> 2 -> 2 -> 4
 
-import { LinkedList } from "./10_LinkedList";
+import type { Node } from './10_LinkedList'
 
-export type Node<T> = {
-  value: T;
-  next?: Node<T>;
-};
+export default function removeDups<T>(head?: Node<T>): Node<T> | undefined {
+  if (!head) return
 
-export default function removeDups<T>(head?: Node<T>): Node<T> | undefined {}
+  let curr = head
+
+  while (curr?.next) {
+    let prev = curr
+    let find: Node<T> | undefined = curr
+
+    while (find) {
+      prev = find
+      find = find.next
+
+      while (find && find.value === curr.value) {
+        // If we find a duplicate, we remove it from the list and check the next one.
+        prev.next = find.next
+        find = find.next
+      }
+    }
+
+    curr = curr.next
+  }
+
+  return head
+}
