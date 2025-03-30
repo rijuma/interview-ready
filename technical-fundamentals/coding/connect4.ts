7c7f76fc8c7f360f1673cc321ecebe131a4b1128
Marcos <rijuma@users.noreply.github.com>
2025-03-30 13:32:28 -0300

    Updated tests on Connect4 to improve coverage

--- technical-fundamentals/coding/connect4.ts
+++ technical-fundamentals/coding/connect4.ts
@@ -13,17 +13,25 @@ export const PLAYER_ONE = 1
 export const PLAYER_TWO = 2
 
 type Board = number[][] // [col][row]
+type Player = number
 
 export class Connect4 {
   #currentPlayer: number
   #rows = 0
   #cols = 0
+  #tokenCount: number
   #board: Board
 
-  constructor({ width = 7, height = 6, startingPlayer = PLAYER_ONE } = {}) {
+  constructor({
+    width = 7,
+    height = 6,
+    startingPlayer = PLAYER_ONE,
+    tokenCount = 4,
+  } = {}) {
     this.#rows = height
     this.#cols = width
     this.#currentPlayer = startingPlayer
+    this.#tokenCount = tokenCount
     this.#board = Array(this.#cols)
       .fill([])
       .map(() => Array(this.#rows).fill(0))
@@ -31,6 +39,8 @@ export class Connect4 {
 
   // Interface methods
   play(col: number): boolean {
+    if (this.winner()) return false // It should not let you play if there's a winner already.
+
     const colIdx = col - 1
     if (this.#board[colIdx] === undefined) return false // Invalid move - Out of bounds
 
@@ -46,7 +56,31 @@ export class Connect4 {
     return true
   }
 
-  winner() {}
+  #checkPiece(colIdx: number, rowIdx: number): Player | null {
+    const token = this.#board[colIdx][rowIdx]
+
+    if (!token) return 0
+
+    // Here we check the piece connections and tell if there's a winner or just continue checking.
+
+    return null
+  }
+
+  winner(): Player | null {
+    let winner: Player | null = null
+
+    this.#board.some((col, colIdx) =>
+      col.some((_, rowIdx) => {
+        const result = this.#checkPiece(colIdx, rowIdx)
+
+        if (result) winner = result
+
+        return result
+      })
+    )
+
+    return winner
+  }
 
   // Debug methods
   getValue(x: number, y: number) {
