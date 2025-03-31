90ba7e17189391a267b158e1b5182cd427e9c1c7
Marcos <rijuma@users.noreply.github.com>
2025-03-30 22:47:56 -0300

    Problem 17

--- technical-fundamentals/coding/problems/17_palindrome.ts
+++ technical-fundamentals/coding/problems/17_palindrome.ts
@@ -1,12 +1,31 @@
-// 7. *Palindrome*:
+// 17. *Palindrome*:
 
 // Implement a function to check if a linked list is a palindrome.
 
-import { LinkedList } from "./10_LinkedList";
+import { type Node } from "./10_LinkedList"
 
-export type Node<T> = {
-  value: T;
-  next?: Node<T>;
-};
+export default function isPalindrome<T>(head: Node<T> | undefined): boolean {
+  if (!head) return false
 
-export default function isPalindrome<T>(head: Node<T> | undefined): boolean {}
+  if (!head.next) return true
+
+  const getPrevious = (node: Node<T>, end?: Node<T>) => {
+    if (node.next === end) return node
+
+    return getPrevious(node.next!, end)
+  }
+
+  const walk = (from: Node<T>, to: Node<T>) => {
+    if (from === to) return true
+
+    if (from.value != to.value) return false
+
+    if (from.next === to && from.next.value === to.value) return true
+
+    const penultimate = getPrevious(from, to)
+    return walk(from.next!, penultimate!)
+  }
+
+  const tail = getPrevious(head)
+  return walk(head, tail)
+}
