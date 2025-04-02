b692712e1ed99a3dde0756f5ba452f345c8ca4f7
Marcos <rijuma@users.noreply.github.com>
2025-04-02 16:03:20 -0300

    Problem 26

--- technical-fundamentals/coding/problems/26_animalShelter.ts
+++ technical-fundamentals/coding/problems/26_animalShelter.ts
@@ -1,41 +1,73 @@
 // 6. *Animal Shelter*:
 
-// An animal shelter, which holds only dogs and cats, operates on a strictly
-// "first in, first out" basis. People must adopt either the "oldest"
-// (based on arrival time) of all animals at the shelter,
+// An animal shelter, which holds only dogs and dogs, operates on a strictly
+// "first in, first out" basis. Peopldogsst adopt either the "oldest"
+// (based on arrival time) of all animal at the shelter,
 // or they can select whether they would prefer a dog or a cat
-// (and will receive the oldest animal of that type).
-// They cannot select which specific animal they would like.
-// Create the data structures to maintain this system and implement operations
 // such as enqueue, dequeueAny, dequeueDog, and dequeueCat.
 // You may use the built-in LinkedList data structure.
 
-export type AnimalType = "dog" | "cat";
+import type { Node } from './10_LinkedList'
+
+export type AnimalType = 'dog' | 'cat'
 
 export class Animal {
-  type: AnimalType;
+  type: AnimalType
+  arrivedAt: Date
+
   constructor(type: AnimalType) {
-    this.type = type;
+    this.type = type
+    this.arrivedAt = new Date()
   }
 }
 
 export default class AnimalShelter {
+  private head?: Node<Animal>
 
-    constructor() {
+  constructor() {}
+
+  enqueue(type: AnimalType): void {
+    const animal = new Animal(type)
+    const node: Node<Animal> = { value: animal }
+    node.next = this.head
+    this.head = node
+  }
+
+  dequeueAny(): Animal | undefined {
+    if (!this.head) return
+
+    const animal = this.head.value
+    this.head = this.head.next
+
+    return animal
+  }
+
+  dequeueDog(): Animal | undefined {
+    return this.dequeueAnimal('dog')
+  }
+
+  dequeueCat(): Animal | undefined {
+    return this.dequeueAnimal('cat')
+  }
+
+  // --
+
+  private dequeueAnimal(type: AnimalType): Animal | undefined {
+    if (!this.head) return
+
+    if (this.head.value.type === type) return this.dequeueAny()
+
+    const extract = (node: Node<Animal> | undefined) => {
+      if (!node?.next) return
+
+      if (node.next.value.type !== type) return extract(node.next)
+
+      const animal = node.next.value
+      node.next = node.next.next
+
+      return animal
     }
 
-    enqueue(type: AnimalType): void {
-
-    }
-
-    dequeueAny(): Animal | undefined {
-
-    }
-
-    dequeueDog(): Animal | undefined {
-    }
-
-    dequeueCat(): Animal | undefined {
-    }
+    return extract(this.head)
+  }
 }
-
