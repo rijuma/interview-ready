7ba0ba969068e851ef9dc2c0d4c3530a7f4195ce
Marcos <rijuma@users.noreply.github.com>
2025-04-02 12:38:11 -0300

    Problem 18

--- technical-fundamentals/coding/problems/18_intersection.ts
+++ technical-fundamentals/coding/problems/18_intersection.ts
@@ -4,14 +4,31 @@
 // Return the first intersecting node. Note that the intersection is defined
 // based on reference, not value.
 
-import { LinkedList } from "./10_LinkedList";
-
-export type Node<T> = {
-  value: T;
-  next?: Node<T>;
-};
+import type { Node } from './10_LinkedList'
 
 export default function intersection<T>(
   list1: Node<T> | undefined,
-  list2: Node<T> | undefined,
-): Node<T> | undefined {}
+  list2: Node<T> | undefined
+): Node<T> | undefined {
+  if (!list1 || !list2) return
+
+  const find = (needle?: Node<T>, haystack?: Node<T>): Node<T> | undefined => {
+    if (!needle || !haystack) return
+
+    if (needle === haystack) return needle
+
+    return find(needle, haystack.next)
+  }
+
+  const walk = (current?: Node<T>): Node<T> | undefined => {
+    if (!current) return
+
+    const link = find(current, list2)
+
+    return link || walk(current.next)
+  }
+
+  const link = walk(list1)
+
+  return link
+}
