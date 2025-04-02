67218ac577effa9dd9603da1dd5f2ac978ea3c92
Marcos <rijuma@users.noreply.github.com>
2025-04-02 13:53:36 -0300

    Problem 22

--- technical-fundamentals/coding/problems/22_stackMin.ts
+++ technical-fundamentals/coding/problems/22_stackMin.ts
@@ -4,20 +4,47 @@
 // Push, pop, and min should all operate in O(1) time.
 //
 
-export default class StackMin<T> {
-    constructor() {
+export default class StackMin<T extends number | string> {
+  private stack: T[]
+  private mins: number[]
 
+  constructor() {
+    this.stack = []
+    this.mins = []
+  }
+
+  push(value: T): void {
+    const idx = this.stack.length
+    this.stack.push(value)
+
+    if (!this.mins.length) {
+      this.mins.push(idx)
+      return
     }
 
-    push(value: T): void {
+    const currentMinIdx = this.mins.at(-1)
+    const min = this.stack[currentMinIdx!]
 
-    }
+    if (value < min) this.mins.push(idx)
+  }
 
-    pop(): T | undefined {
+  pop(): T | undefined {
+    const idx = this.stack.length - 1
 
-    }
+    if (idx < 0) return
 
-    min(): T | undefined {
+    const output = this.stack.pop()
 
-    }
+    if (this.mins?.at(-1) === idx) this.mins.pop()
+
+    return output
+  }
+
+  min(): T | undefined {
+    if (!this.mins.length) return
+
+    const minIdx = this.mins.at(-1)
+
+    return this.stack[minIdx!]
+  }
 }
